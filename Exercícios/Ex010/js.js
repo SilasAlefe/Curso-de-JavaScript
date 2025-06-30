f00b4f3b2e0aa5345019ca7924fccac8a4370fab
Silas Álefe <116173586+SilasAlefe@users.noreply.github.com>
2025-06-30 16:47:29 -0300

    Atualização 30/06/2025 - 16:47

---
+++ Exercícios/Ex010/js.js
@@ -0,0 +1,12 @@
+var bt1 = document.querySelector('#bt1')
+var bt2 = document.querySelector('#bt2')
+var bt3 = document.querySelector('#bt3')
+var bt4 = document.querySelector('#bt4')
+
+bt1.addEventListener('click', clicou1)
+bt2.addEventListener('click', clicou2)
+bt3.addEventListener('click', clicou3)
+bt4.addEventListener('click', clicou4)
+
+
+document.write(`testando ${bt1}`)
