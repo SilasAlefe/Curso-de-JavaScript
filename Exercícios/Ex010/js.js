2acc424528a4f44e0dd5d4b2f5eb58666b0cb457
Silas Álefe <116173586+SilasAlefe@users.noreply.github.com>
2025-06-30 17:41:34 -0300

    Atualização 30/06/2025 - 17:41

--- Exercícios/Ex010/js.js
+++ Exercícios/Ex010/js.js
@@ -10,15 +10,18 @@ bt4.addEventListener('click', clicou4)
 
 
 function clicou1(){
-    var txt = document.querySelector('#txt')
-    txt.innerHTML = ('Clicou no primeiro botão')
+    var texto = document.getElementById('txt')
+    texto.innerHTML += '<p>Clicou no primeiro botão</p>'
 }
 function clicou2(){
-
+    var texto = document.getElementById('txt')
+    texto.innerHTML += '<p>Clicou no Segundo botão</p>'
 }
 function clicou3(){
-
+    var texto = document.getElementById('txt')
+    texto.innerHTML += '<p>Clicou no terceiro botão</p>'
 }
 function clicou4(){
-
+    var texto = document.getElementById('txt')
+    texto.innerHTML += '<p>Clicou no quarto botão</p>'
 }
