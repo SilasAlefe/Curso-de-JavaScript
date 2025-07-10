a269094156c04f2851fec133fdb6154e35781f83
Silas Álefe <116173586+SilasAlefe@users.noreply.github.com>
2025-07-10 17:41:55 -0300

    Atualização 10/07/2025 - 17:41

--- Aula 14/ambiente.js
+++ Aula 14/ambiente.js
@@ -2,12 +2,12 @@
 var c = 1
 while (c <= 5){
     console.log(c)
-    c==
+    c++
 }
 */
 //---------------------
-
-for(var c = 1; c<=10; c=c+1){
-    console.log(`Passo ${c}`)
+console.log("Vai começar...")
+for ( var c = 1; c <= 5; c++ ) {
+    console.log(c)
 }
-console.log('Fim')
+console.log("FIM!")
